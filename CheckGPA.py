5e0dd38cbcc8f29579b1d2f7d138f1f4034eb83f
km3r4 <152334032+km3r4@users.noreply.github.com>
2023-11-30 13:05:29 -0700

    Update CheckGPA.py
    Calculates your GPA using the grades you have in your classes..

--- CheckGPA.py
+++ CheckGPA.py
@@ -1,54 +1,30 @@
-
-def sub_grade(sub):
-    global total
-    global fail
-    if sub== 'A+' or sub== 'a+':
-        total+=5
-    elif sub=='A' or sub== 'a':
-        total+=4
-    elif sub=='B' or sub=='b':
-        total+=3
-    elif sub=='C' or sub=='c':
-        total+=2
-    elif sub=='D' or sub=='d':
-        totatl+=1
-    elif sub=='F' or sub=='f':
-        totatl=fail
+classno=None
+while classno==None:
+    try:
+        classno=int(input("How many class are you taking? "))
+    except:
+        print("Invalid. Please enter a number.\n")
+output=0
+i=0
+while i<classno:
+    grade=input("Enter your letter grade for Class %s. " %(str(i+1)))
+    grade=grade.upper()
+    if grade== 'A+' or grade== 'A' or grade== 'B' or grade== 'C' or grade== 'D' or grade== 'F':
+        i+=1
+        if grade== 'A+':
+            output+=5
+        elif grade== 'A':
+            output+=4
+        elif grade== 'B':
+            output+=3
+        elif grade== 'C':
+            output+=2
+        elif grade== 'D':
+            output+=1
+        elif grade== 'F':
+            output+=0
+        print(output)
     else:
-        print("Invalid input")
-    return total
-
-total=0
-fail=0
-
-
-#def CheckGrade():
-grade1=input("What is your 1st period grade? ")
-grade2=(input("What is your 2nd period grade? "))
-grade3=(input("What is your 3rd period grade? "))
-grade4=(input("What is your 4th period grade? "))
-grade5=(input("What is your 5th period grade? "))
-
-#CheckGrade()
-sub_grade(grade1)
-sub_grade(grade2)
-sub_grade(grade3)
-sub_grade(grade4)
-sub_grade(grade5)
-
-if (grade1 or grade2 or grade3 or grade4 or grade5) in ('F' or 'f'):
-    total=fail
-
-gpa=total/5
-
-if gpa>=5:
-    gpa=5
-
-if gpa ==0:
-    print("Unfortunately, you have failed this semester! ")
-    print("Your current GPA is ",gpa)
-elif gpa >=0:
-    print("Keep up the good work!")
-    print("Your current GPA is ",gpa)
-else:
-    print("Please try again")
+        print("Error. Please enter a valid letter grade (A, B, C, D, F)\n")
+average="%.2f" % (float(output)/float(classno))
+print("Your current GPA is: %s " %(str(average)))
